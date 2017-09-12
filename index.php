3324408b3d1b05b948a842e79faf96caf2261ee2
Sakvad <dxe8484@rit.edu>
2017-09-12 17:32:45 -0400

    Changed wording for Swedish card.

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
 
                 <a class="card-link" href="https://github.com/Dellman/lendandlease.app.git" target="_blank"><div class="card">
                   <h4>Sweden JavaScript Slideshow</h4>
-                  <p>This was a project I did for fun just to explore some nuisances of JavaScript and experiment with various jQuery plugins. The different sized images made it hard to work with, especially with how many they are, and I hope to make an improved version at some point.</p>
+                  <p>I did this project for fun to explore some nuances of JavaScript and experiment with various jQuery plugins.</p>
                 </div></a>
                 <a class="card-link" href="https://github.com/Dellman/lendandlease.app.git" target="_blank"><div class="card">
                     <h4>Lend and Lease</h4>
