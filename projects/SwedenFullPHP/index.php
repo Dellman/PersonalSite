97a892fc0347077c5611688cc5894c8fb7a2ba14
Sakvad <dxe8484@rit.edu>
2017-09-22 13:00:29 -0400

    Moved jquery and scrolling script to head to load first to create a smoother experience.

--- projects/SwedenFullPHP/index.php
+++ projects/SwedenFullPHP/index.php
@@ -9,6 +9,12 @@
     <link rel="stylesheet" href="css/styles.css">
     <!-- Custom Font from Google -->
     <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab" rel="stylesheet">
+
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Fullpage JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.min.js"></script>
   </head>
   <body>
     <nav id="top-nav">
@@ -83,11 +89,6 @@
     <nav id="div-dot-container"></nav>
 
     <footer>
-      <!-- jQuery -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-      <!-- Fullpage JS -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.4/jquery.fullpage.min.js"></script>
 
       <!-- My JS -->
       <script type="text/javascript"  src="js/script.js"></script>
