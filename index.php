03992654f081c3334289be2eea7989eefdee74c2
Sakvad <dxe8484@rit.edu>
2017-09-27 18:29:53 -0400

    Further attempts on getting cards to look as desired. No progress, may revert back.

--- index.php
+++ index.php
@@ -60,14 +60,27 @@
                 <h2 id="project-bottom">Projects</h2>
 
                 <!-- Cards for projects -->
-                <a class="card-link" href="projects/SwedenFullPHP/" target="_blank"><div class="card" id="slideShowBG">
+                <!-- <a class="card-link" href="projects/SwedenFullPHP/" target="_blank"><div class="card" id="slideShowBG"> -->
                     <!-- <div class="card-front">
                         <img id="project1" src="images/SlideshowCard.png" alt="Sweden Slideshow Page">
                     </div> -->
-                  <div class="card-back"><h4>Sweden JavaScript Slideshow</h4>
+                  <!-- <div class="card-back"><h4>Sweden JavaScript Slideshow</h4>
                   <p>I did this project for fun to explore some nuances of JavaScript and experiment with various jQuery plugins.</p></div>
-                </div></a>
-                <a class="card-link" href="https://github.com/Dellman/lendandlease.app.git" target="_blank"><div class="card" id="lendAndLeaseBG">
+                </div></a> -->
+
+                <!-- <div class="project-container">
+                    <a class="project-link" href="projects/SwedenFullPHP/" target="_blank">
+                        <div class="project-img">
+                            <img src="images/SlideshowCard.png" alt="Sweden Slideshow Page">
+                        </div>
+                        <div class="project-text">
+                            <h4>Sweden JavaScript Slideshow</h4>
+                            <p>I did this project for fun to explore some nuances of JavaScript and experiment with various jQuery plugins.</p>
+                        </div>
+                    </a>
+                </div> -->
+
+                <!-- <a class="card-link" href="https://github.com/Dellman/lendandlease.app.git" target="_blank"><div class="card" id="lendAndLeaseBG"> -->
                     <!-- <div class="card-front">
                         <img src="" alt="">
                     </div> -->
@@ -75,14 +88,14 @@
                     <p>This was a group project during my study abroad in Sweden. With a team of three, we created a website that allows people to put up items for other people in the neighborhood to borrow and lease items. While we did not implement all of our ideal functions, we got a majority of the primary functions. We used Node.JS with Passport for sessions and authentication, and Express to connect to our MySQL database.
                     </p></div>
                 </div></a>
-                <a class="card-link" href="projects/ClientProject1/index.html" target="_blank"><div class="card" id="project1BG">
+                <!-- <a class="card-link" href="projects/ClientProject1/index.html" target="_blank"><div class="card" id="project1BG"> -->
                     <!-- <div class="card-front">
-                        <img src="images/Project1Card2.png" alt="Dynamic JavaScript Page">
+                        <img src="images/Project1Card.png" alt="Dynamic JavaScript Page">
                     </div> -->
                     <div class="card-back"><h4>Dynamic JavaScript Project (2016)</h4>
                     <p>Using pure JavaScript, I created a website to browse characters from the game Overwatch. They can be sorted by role and difficulty, and clicking on a character will take the user to the character's official page. All of the information is stored in a JSON file. All images, select menus, options, and some text are dynamically created. Lastly, this project uses cookies and local storage and gives suggestions of modern browsers to those using older browsers.</p></div>
                 </div></a>
-                <a class="card-link" href="http://strategicinterests.com/" target="_blank"><div class="card" id="strategicInterestsBG">
+                <!-- <a class="card-link" href="http://strategicinterests.com/" target="_blank"><div class="card" id="strategicInterestsBG"> -->
                     <!-- <div class="card-front">
                         <img src="images/StrategicInterestsCard2.png" alt="Stragic Interests Page">
                     </div> -->
