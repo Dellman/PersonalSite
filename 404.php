cf57cb3093de56979380a84b11b6fde8f86bb6cf
Sakvad <dxe8484@rit.edu>
2017-10-02 14:58:43 -0400

    Made the 404 image absolute.

--- 404.php
+++ 404.php
@@ -1,11 +1,8 @@
 <?php
     require_once("phpTemplates/header.php");
 ?>
-<!-- <div style="background-image: url(images/mb1.jpg); width: 100vw; height: 100vh;" class="">
-
-</div> -->
 <h2>Hello traveler! Unfortunately, there is nothing here. <a href="https://www.danielellman.com">Head on back to the content!</a></h2>
-<img src="images/MissingBoat.jpg" style="width: 100vw; height: 80vh" alt="Missing Boat">
+<img src="https://www.danielellman.com/images/MissingBoat.jpg" style="width: 100vw; height: 80vh" alt="Missing Boat">
 <p style="text-align:center">Or just enjoy the view...</p>
 
 <?php
