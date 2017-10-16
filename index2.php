c0bdf4f1cd23b4653f2a01332103b1bdb81a0617
Sakvad <dxe8484@rit.edu>
2017-10-16 15:38:57 -0400

    Minified images further, working on new index page

---
+++ index2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <style media="screen">
+      body{
+        margin: 0;
+        padding: 0;
+      }
+      div > img{
+        width: 50%;
+        display: block;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="">
+      <!-- <img src="images/stonehedge.jpg" alt=""> -->
+      <p>Hey there, I'm Daniel.</p>
+      <br />
+      <p>I'm a recent graduate from <a href="#">Rochester Institute of Technology.</a> </p>
+    </div>
+  </body>
+</html>
