eb52f3c56b6604ad1ce81b07e70e20e326c54796
Sakvad <dxe8484@rit.edu>
2017-10-18 15:37:29 -0400

    Reverted styles in header.php back to styles.css

--- phpTemplates/header.php
+++ phpTemplates/header.php
@@ -13,54 +13,5 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/responsiveStyles.css">
     <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-99088303-1', 'auto'); ga('send', 'pageview');</script>
-    <style media="screen">
-      nav
-      {
-        background-color: #a5a5a5;
-        position: fixed;
-        top: 0;
-        width: 100%;
-        z-index: 999;
-        font-size: 1.25em;
-      }
-      nav ul
-      {
-        display: flex;
-        justify-content: space-around;
-        margin-bottom: 0;
-        padding: 0;
-      }
-      nav ul li{
-        padding: .5em;
-        list-style: none;
-        font-size: 1.1em;
-      }
-      nav ul li a{
-        color: #212121;
-        text-decoration: none;
-        font-size: 1em;
-      }
-      nav a{
-        color: #212121;
-      }
-      nav ul li a:focus{
-        text-decoration: none;
-      }
-      nav ul li a:hover{
-        text-decoration: none;
-      }
-      .active, nav ul li a:hover{
-        color:  #23527c;
-      }
-      body{
-        display: block;
-        background-color: #FFFFFF;
-        font-family: 'Raleway', sans-serif, "Verdana", "Georgia", "Arial";
-        color: #000000;
-      }
-      h1, h2, h3, h4, h5, h6{
-        font-family: 'Playfair Display', serif;
-      }
-    </style>
   </head>
   <body>
